0855bb055deaf1cdb3b1ed91a6ee34718b2deb2a
MAPORTER16 <matthewporter2000@gmail.com>
2025-09-04 18:41:14 -0600

    l

---
+++ scripts/getdates.js
@@ -0,0 +1,3 @@
+document.getElementById("currentyear").textcontent = `©${new Date().getFullYearYear()} ⚔️Matthew Porter⚔️ UTAH⛰️ `;
+
+document.getElementById("LastModified").textContent = `Last Modification: ${document.lastModified}`;
