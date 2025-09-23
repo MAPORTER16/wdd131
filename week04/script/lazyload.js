d09d4c804f11d70eb773389ccfe7de151f69ab50
MAPORTER16 <matthewporter2000@gmail.com>
2025-09-23 16:50:14 -0600

    done

---
+++ week04/script/lazyload.js
@@ -0,0 +1,3 @@
+document.getElementById("currentyear").textcontent = `©${new Date().getFullYear()} ⚔️Matthew Porter⚔️ UTAH⛰️ `;
+
+document.getElementById("LastModified").textContent = `Last Modification: ${document.lastModified}`;
